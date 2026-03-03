1ea299c19b6542dbdb1f1a69daf833cf704b3588
flavius841 <flaviusepintilie@gmail.com>
2026-03-03 19:13:56 +0200

    readme update

--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="trackgen",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=["requests"],
+
+    install_requires=[
+        "requests",
+        "colorama",
+        "prompt_toolkit"
+    ],
+
     entry_points={
         "console_scripts": [
             "trackgen=CLI_trackGenerator.main:main",
         ],
     },
+
+    author="Your Name",
+    description="CLI music discovery tool using MusicBrainz API",
+    python_requires=">=3.8",
 )
