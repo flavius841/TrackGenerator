58b6c7b1dc9514a6ff6127332a9c9fd9d1442c98
flavius841 <flaviusepintilie@gmail.com>
2026-03-03 09:01:01 +0200

    more songs

--- CLI_trackGenerator/main.py
+++ CLI_trackGenerator/main.py
@@ -1,14 +1,25 @@
 import sys
 import requests
 from colorama import init, Fore, Style as CStyle
+from prompt_toolkit import prompt
+from prompt_toolkit.styles import Style as PTStyle
 
 
 BASE_URL = "https://musicbrainz.org/ws/2/recording?query=tag:{genre}&fmt=json"
+
+style = PTStyle.from_dict({
+    "prompt": "ansigreen dim",
+    "": "bold ansibrightgreen",
+})
+
+minn = 0
+maxx = 10
 
 
 def search_by_genre(genre):
 
     response = requests.get(BASE_URL.format(genre=genre))
+    verify = False
 
     if response.status_code != 200:
         print("Error fetching data")
@@ -22,7 +33,7 @@ def search_by_genre(genre):
         print("No tracks found.")
         return
 
-    for i, track in enumerate(tracks_id[:10], start=1):
+    for i, track in enumerate(tracks_id[minn:maxx], start=1):
         title = track["title"]
         artist = track["artist-credit"][0]["name"]
 
@@ -31,6 +42,11 @@ def search_by_genre(genre):
 
         else:
             print(f"{CStyle.BRIGHT}{Fore.GREEN}{i}. {Fore.YELLOW}{title}{CStyle.RESET_ALL} – {CStyle.BRIGHT}{Fore.CYAN}{artist}{CStyle.RESET_ALL}")
+
+        if verify:
+            print(
+                f"{CStyle.BRIGHT}{Fore.RED}No or no more tracks found for genre: {genre}{CStyle.RESET_ALL}")
+            break
 
 
 def main():
@@ -49,11 +65,33 @@ def main():
     if len(sys.argv) < 2:
         print(f"{CStyle.BRIGHT}{Fore.YELLOW}Welcome to {Fore.GREEN}TrackGenerator{Fore.YELLOW}. Type <help> for more information.{CStyle.RESET_ALL}")
         while True:
-            command = input(">> ")
+            command = prompt(
+                [("class:prompt", ">>> ")],
+                style=style,
+                # completer=command_completer,
+            ).strip()
             if command.lower() == 'exit':
                 print("Goodbye!")
                 break
-            search_by_genre(command)
+            elif command.lower() == 'help':
+                print(
+                    f"{CStyle.BRIGHT}{Fore.YELLOW}Usage: {Fore.GREEN}trackgen <genre>{CStyle.RESET_ALL}")
+                print(
+                    f"{CStyle.BRIGHT}{Fore.YELLOW}Example: {Fore.GREEN}trackgen rock{CStyle.RESET_ALL}")
+
+            elif command.lower() == 'more':
+                if 'previous_command' not in locals():
+                    print(
+                        f"{CStyle.BRIGHT}{Fore.RED}No previous command found. Please enter a genre first.{CStyle.RESET_ALL}")
+                    continue
+                global minn, maxx
+                minn += 10
+                maxx += 10
+                search_by_genre(previous_command)
+
+            else:
+                search_by_genre(command)
+                previous_command = command
 
     genre = sys.argv[1]
     search_by_genre(genre)
